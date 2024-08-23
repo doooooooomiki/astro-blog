e5d570102122d7cb21c63c720d6308f33c3a1942
doooooooomiki <dominic.kirste@gmail.com>
2024-08-23 22:47:32 +0200

    ➕

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), markdoc(), keystatic()],
-  output: 'hybrid',
+  integrations: [mdx(), sitemap(), react(), markdoc(), keystatic(), tailwind()],
+  output: 'hybrid'
 });
